e9985117562c0e3b2d7bfcb9a0c2ab267e409502
developersview <pcslg1998@gmail.com>
2022-06-10 13:55:46 +0530

    Updated previous practice code

---
+++ app.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "Paragraph changed by Pranoy";
+}
+
+function myFunction2() {
+    alert("Hello Pranoy");
+    [1, 2, 3].forEach(alert);
+}
+
+function myFunction3() {
+    let myName = prompt("What is your name?");
+    //console.log(myName);
+    alert(myName);
+}
+
+function myFunction4() {
+    let msg = "Welcome";
+    let name = "Pranoy";
+    alert(msg + " " + name);
+}
---
+++ basics.js
@@ -0,0 +1,17 @@
+let a = 45;
+a = 45 + 89;
+let b = "This is Pranoy";
+const c = 67;
+console.log(a);
+console.log(b);
+// c = 66;
+console.log(c);
+
+function myFunction4() {
+    let msg = "Welcome";
+    let name = "Pranoy";
+    console.log(msg + " " + name);
+    console.log(msg.length);
+}
+
+myFunction4();
