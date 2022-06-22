4e95c8826a911d3082835ddda2131c6c6b97a960
developersview <pcslg1998@gmail.com>
2022-06-22 14:13:30 +0530

    completed values and variables

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
 let js = 'Amazing';
 // if (js === 'Amazing') alert("JS is fun!!");
 console.log(58 + 98 + 62 - 78);
+
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+let firstName = "Matilda";
+let PI = 3.1415;
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(PI);
+
+//Assignement - Values and Variables
+let country = 'INDIA';
+let continent = 'Asia';
+let population = 98956497662;
+
+console.log(country + " " + continent + " " + population);
