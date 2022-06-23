f2a396f5d2967312313ac5303b88ca2f4fcf3497
developersview <pcslg1998@gmail.com>
2022-06-23 14:27:47 +0530

    completed data types lecture

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+/* Values and Variables */
+/* -------------------------------------------------- */
 let js = 'Amazing';
 // if (js === 'Amazing') alert("JS is fun!!");
 console.log(58 + 98 + 62 - 78);
@@ -18,3 +20,27 @@ let continent = 'Asia';
 let population = 98956497662;
 
 console.log(country + " " + continent + " " + population);
+
+/* Data Types */
+/* -------------------------------------------------- */
+//Object
+let person = {
+    name: 'Jonas',
+    age: 24
+};
+console.log(person);
+console.log(typeof person);
+console.log(typeof person.name);
+console.log(typeof person.age);
+//boolean
+let isEmpty = true;
+console.log(isEmpty);
+console.log(typeof isEmpty);
+//undefined
+let salary;
+console.log(salary);
+console.log(typeof salary);
+//null
+let i = null;
+console.log(i);
+console.log(typeof i);
