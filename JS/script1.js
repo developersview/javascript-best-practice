6b470dcb6f327db4dd66422c64b3349ef8352ff7
developersview <pcslg1998@gmail.com>
2022-08-03 13:51:44 +0530

    activating strict mode

---
+++ JS/script1.js
@@ -0,0 +1,9 @@
+/* Activating Strict mode */
+"use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+
+if (hasDriverLicense) console.log("I can drive!");
