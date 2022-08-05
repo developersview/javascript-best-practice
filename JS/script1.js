78cda7e1e5fa984bd93a7953dc213aa003db63db
developersview <pcslg1998@gmail.com>
2022-08-05 13:53:00 +0530

    working with Functions

--- JS/script1.js
+++ JS/script1.js
@@ -1,4 +1,5 @@
 /* Activating Strict mode */
+/* ---------------------------------------------------- */
 "use strict";
 
 let hasDriverLicense = false;
@@ -7,3 +8,24 @@ const passTest = true;
 if (passTest) hasDriverLicense = true;
 
 if (hasDriverLicense) console.log("I can drive!");
+
+/* Working with Functions */
+/* ---------------------------------------------------- */
+function fruitProcessor(apple, orange) {
+    console.log(apple, orange);
+    let Order;
+    if (apple === 0 && orange === 0) {
+        Order = "Can't make juice!";
+    } else if (apple === 1 && orange === 1) {
+        Order = `Order : Juice with ${apple} apple and ${orange} orange`;
+    } else if (apple > 1 && orange === 1) {
+        Order = `Order : Juice with ${apple} apples and ${orange} orange`;
+    } else if (apple === 1 && orange > 1) {
+        Order = `Order : Juice with ${apple} apple and ${orange} oranges`;
+    } else {
+        Order = `Order : Juice with ${apple} apples and ${orange} oranges`;
+    }
+    return Order;
+}
+const result = fruitProcessor(5, 4);
+console.log(result);
